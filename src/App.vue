707d898902919a8d26f1a4ca573facd7b08d9e75
oliv-bernier <olivbernier@gmail.com>
2022-06-07 22:58:12 +0200

    remove unnecessary log

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,6 @@
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
 
-console.log('toto =>', process.env.VUE_APP_API_URL);
-
 export default {
   name: 'App',
   components: {
